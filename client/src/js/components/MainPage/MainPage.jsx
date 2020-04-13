fc0990bee7c05d7ce9afac11c5c1a135e9f35fa3
Ryan Lypps <ryanlypps@Ryans-MacBook-Pro.local>
2020-04-12 19:22:08 -0700

    animations

--- client/src/js/components/MainPage/MainPage.jsx
+++ client/src/js/components/MainPage/MainPage.jsx
@@ -3,14 +3,29 @@ import { connect } from 'react-redux';
 import Header from '../Header/Header';
 
 class MainPage extends Component {
+  constructor() {
+    super();
+
+    this.keyframes = this.keyframes.bind(this);
+  }
+
+
+  keyframes() {
+  
+  }
 
   render() {
     return (
       <div className="container">
-        <Header />
-        <button
-          className='hire'>Hire me</button>
-        <div className='second'>Thank you!</div>
+        <div className="row">
+          <div className="col-lg-6">
+            <Header />
+          </div>
+          <div className="col-lg-6">
+            <button className='hire' onClick={this.keyframes}>Hire me</button>
+            {/* <div className='pulse'>Ciera</div> */}
+          </div>
+        </div>
       </div>
     );
   }
