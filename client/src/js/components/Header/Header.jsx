ede57862d78cefd169858ed7987c15f4337a4327
Ryan Lypps <ryanlypps@ryans-mbp.attlocal.net>
2020-04-24 20:55:00 -0700

    Got the image carousel to mostly work. Just need right photos

--- client/src/js/components/Header/Header.jsx
+++ client/src/js/components/Header/Header.jsx
@@ -6,18 +6,22 @@ class Header extends Component {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-lg-12" style={{ marginTop: '-17em' }}>
-            <div style={{ position: 'relative', textAlign: 'center', top: '61%', marginLeft: '82px' }}>
+          <div className="col-lg-12" style={{}}>
+            <div style={{ position: 'relative', textAlign: 'center', marginLeft: '82px' }}>
               <span className='header1'>Ryan  </span>
               <span className='header2'>Lypps</span>
             </div>
-            <div style={{ textAlign: 'center', top: '61%', position: 'relative', marginLeft: '20px' }}>
+            <div style={{ textAlign: 'center', position: 'relative', marginLeft: '20px' }}>
               <h2 style={{ color: 'black' }}>Full Stack Web Developer</h2>
             </div>
-            <div id='cf'>
-              <div id='cf3'>
-              <img className="bottom rounded " src='http://www.tcort.com/wp-content/uploads/yootheme/features_layout-wrapper.jpg' style={{ width:'1200px', height: '720px'}} />
-              <img className="top rounded" src="http://www.tcort.com/wp-content/uploads/yootheme/features_slideshow-fullscreen.jpg" style={{ width:'1200px', height: '720px' }} />
+            <div className="content">
+              <div className="gallery-item slider-container">
+                <div className="slide-holder">
+                  <img src="http://www.tcort.com/wp-content/uploads/yootheme/features_layout-wrapper.jpg" alt="" />
+                  <img src="https://png.pngtree.com/png-clipart/20190520/original/pngtree-cute-ghost-ghostly-cute-ghost-halloween-halloween-ghost-png-image_3933462.jpg" alt="" />
+                  <img src="http://www.tcort.com/wp-content/uploads/yootheme/features_slideshow-fullscreen.jpg" alt="" />
+                  <img src="http://lorempixel.com/output/cats-q-c-640-480-2.jpg" alt="" />
+                </div>
               </div>
             </div>
           </div>
