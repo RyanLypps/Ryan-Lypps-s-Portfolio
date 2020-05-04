8753d4c16acfdf6e68f392aaede3a68541a2c994
ryan lypps <r_lypps@yahoo.com>
2020-05-04 12:30:37 -0700

    moved header into its own file

---
+++ client/src/js/components/ImageCarousel/ImageCarousel.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import StreetEatzBlack from '../../../assets/StreetEatzBlack.png';
+import WeatherNow from '../../../assets/WeatherNow.png';
+import ToDoApp from '../../../assets/ToDoApp.png';
+import GymBuddy from '../../../assets/GymBuddy.png';
+
+class ImageCarousel extends Component {
+
+  render() {
+    return (
+      <div className="container" style={{ height: '75%'}} >
+          <div className="content">
+            <div className='sub-headers' style={{ position: 'relative', textAlign: 'center'}}>
+              <h2 className='lypps' style={{ color: 'white'}}>Full Stack Developer</h2>
+            </div>
+          <div className="gallery-item" style={{height: '100%', width: '100%'}}>
+            <div className="slide-holder" >
+              <img src={StreetEatzBlack} alt="" className='ok image1' style={{  }} />
+              <img src={GymBuddy} alt="" className='ok image1' style={{  }} />
+              <img src={ToDoApp} alt="" className='ok' style={{  }} />
+              <img src={WeatherNow} alt="" className='ok' style={{  }} />
+            </div>
+          </div>
+      </div>
+      </div >
+    );
+  }
+}
+
+export default ImageCarousel;
