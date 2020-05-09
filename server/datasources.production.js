1b18532647e354fa82ab72228674230feec0d9ba
ryan lypps <r_lypps@yahoo.com>
2020-05-08 20:57:31 -0700

    ok

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -2,7 +2,14 @@
 
 module.exports = {
   'mongoatlas': {
+    'host': '',
+    'port': process.env.PORT,
     'url': process.env.MONGODB_URI,
+    'database': 'mongodb',
+    'password': '12345',
+    'name': 'mongoatlas',
+    'user': 'ghoster',
+    'useNewUrlParser': true,
     'connector': 'mongodb',
   },
 };
