dac5b6234f473b113505d981d57a7279096e129a
ryan lypps <r_lypps@yahoo.com>
2020-05-08 21:30:23 -0700

    ok

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   'mongoatlas': {
-    'host': '',
-    'port': 0,
+    'host': 'mongodb://ghoster:12345@my-portfolio-shard-00-00-1zgcd.mongodb.net',
+    'port': 27017,
     'url': process.env.MONGODB_URI,
     'database': 'mongodb',
     'password': '12345',
